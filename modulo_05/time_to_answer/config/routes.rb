176363dde2c27c4dce87937d6bf6ba1f49246a89
Marcelo Campos <marcelofillipe98@gmail.com>
2019-01-21 14:32:35 -0200

    Feat:   add devise for Admin and Profile   add I18n in login

--- modulo_05/time_to_answer/config/routes.rb
+++ modulo_05/time_to_answer/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :profiles_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :admins
+  devise_for :profiles
   get 'welcome/index'
   root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
