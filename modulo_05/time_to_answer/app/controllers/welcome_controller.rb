af1a923a8150741edf6f01a1983a32c5ebd9f97e
Marcelo Campos <marcelofillipe98@gmail.com>
2019-01-23 12:02:06 -0200

    feat: Cria herança com views de admins, profiles e página principal. feat: Use before_action for admins e profiles.

--- modulo_05/time_to_answer/app/controllers/welcome_controller.rb
+++
@@ -1,8 +0,0 @@
-class WelcomeController < ApplicationController
-  # http_basic_authenticate_with name: "Marcelo", password: "123"
-  before_action :authenticate_admin!
-  before_action :authenticate_profile!
-
-  def index
-  end
-end
